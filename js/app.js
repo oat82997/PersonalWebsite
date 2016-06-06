b8de5dc45efe9f6d6b996d60788e05c41e0a6b70
oat82997 <oat82997@yahoo.com>
2016-06-06 01:25:51 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,28 +1,41 @@
 $(document).ready(function(){
-    $(".menu1a").click(function(){
-        $(".menu1b").toggle();
-        $(".menu2b").hide();
-        $(".menu3b").hide();
-        $(".menu4b").hide();
-    });
-    $(".menu2a").click(function(){
-        $(".menu2b").toggle();
-        $(".menu1b").hide();
-        $(".menu3b").hide();
-        $(".menu4b").hide();
-    });
-    $(".menu3a").click(function(){
-        $(".menu3b").toggle();
-        $(".menu2b").hide();
-        $(".menu1b").hide();
-        $(".menu4b").hide();
-    });
-    $(".menu4a").click(function(){
-        $(".menu4b").toggle();
-        $(".menu2b").hide();
-        $(".menu3b").hide();
-        $(".menu1b").hide();
-    });
-    var e=Math.ceil(6*Math.random());
-    $("body").removeClass().addClass("color-scheme-"+e)
+  $(".menu1a").click(function(){
+      $(".menu1b").show();
+      $(".menu2b").hide();
+      $(".menu3b").hide();
+      $(".menu4b").hide();
+      $(".thanks").hide();
+  });
+  $(".menu2a").click(function(){
+      $(".menu2b").show();
+      $(".menu1b").hide();
+      $(".menu3b").hide();
+      $(".menu4b").hide();
+      $(".thanks").hide();
+  });
+  $(".menu3a").click(function(){
+      $(".menu3b").show();
+      $(".menu2b").hide();
+      $(".menu1b").hide();
+      $(".menu4b").hide();
+      $(".thanks").hide();
+  });
+  $(".menu4a").click(function(){
+      $(".menu4b").show();
+      $(".menu2b").hide();
+      $(".menu3b").hide();
+      $(".menu1b").hide();
+      $(".thanks").hide();
+  });
+
+  var n = Math.ceil(6 * Math.random());
+  $("body").removeClass().addClass("colors-" + n);
+
 });
+
+var n = 0;
+
+function changeColor() {
+  if(n >= 6){n = 1;}else{n = n + 1;}
+  $("body").removeClass().addClass("colors-" + n);
+};
